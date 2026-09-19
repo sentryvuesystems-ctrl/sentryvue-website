593069eac926233585a2a2fc32e563367d32a28e
sentryvuesystems-ctrl <317438133+sentryvuesystems-ctrl@users.noreply.github.com>
2026-09-19 19:16:40 +0000

    Remove Vercel-incompatible tracing root override

--- next.config.js
+++ next.config.js
@@ -6,9 +6,6 @@ const nextConfig = {
   output: process.env.NEXT_OUTPUT_MODE,
   poweredByHeader: false,
   productionBrowserSourceMaps: false,
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
